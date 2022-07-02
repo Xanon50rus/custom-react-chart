9e1943173a8e2fec38054f33bae8568cd8964c4b
Igor Kirnosov <xan50rus@gmail.com>
2022-07-02 13:44:46 +0200

    added axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 import {
   Chart as ChartJS,
@@ -37,29 +38,41 @@ export const options = {
   },
 };
 
-// const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
 
-// export const data = {
-//   labels,
-//   datasets: [
-//     {
-//       fill: true,
-//       label: 'Dataset 2',
-//       data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-//       borderColor: 'rgb(53, 162, 235)',
-//       backgroundColor: 'rgba(53, 162, 235, 0.5)',
-//     },
-//   ],
-// };
 
 function App() {
-  const getData = async () => {
-    let apiResult = await fetch(
-      'https://api.multifarm.fi/jay_flamingo_random_6ix_vegas/get_assets?pg=1&tvl_min=50000&sort=tvlStaked&sort_order=desc&farms_tvl_staked_gte=10000000'
-    );
+  const [config, setConfig] = useState({
+    labels,
+    datasets: [
+      {
+        fill: true,
+        label: 'Dataset 2',
+        data: {},
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  });
 
-    apiResult = await apiResult.json();
+  const getData = async () => {
+    let apiResult = await axios.get('https://api.multifarm.fi/jay_flamingo_random_6ix_vegas/get_assets?pg=1&tvl_min=50000&sort=tvlStaked&sort_order=desc&farms_tvl_staked_gte=10000000');
+
+    apiResult = apiResult.data as any;
+    
     console.log(apiResult);
+    setConfig({
+      labels,
+      datasets: [
+        {
+          fill: true,
+          label: 'Dataset 2',
+          data: apiResult,
+          borderColor: 'rgb(53, 162, 235)',
+          backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        },
+      ],
+    })
   };
 
   useEffect(() => {
@@ -67,7 +80,9 @@ function App() {
   }, []);
 
   return (
-    <div className="App">{/* <Line options={options} data={data} />; */}</div>
+    <div className="App">
+      <Line options={options} data={config} />
+    </div>
   );
 }
 
